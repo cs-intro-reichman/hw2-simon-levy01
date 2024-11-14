743e53a08e67c75c6ad58b8172e91a2338ab623f
Simon Levy <simon.levy01@post.runi.ac.il>
2024-11-14 17:04:45 +0200

    finished assignment 4

--- Cheers.java
+++ Cheers.java
@@ -5,14 +5,14 @@ public class Cheers {
             String wordUpper = args[0].toUpperCase();
             int length = wordUpper.length();
             String anLetters = "AEFHILMNORSX";
-            for (int i = 0; i < length; i++){
+            for (int i = 0; i < length; i++){ //compare each letter to anLetters and print a or an as needed
                 if (anLetters.indexOf(wordUpper.charAt(i)) != -1){
                      System.out.println("Give me an " + wordUpper.charAt(i) + ": " + wordUpper.charAt(i) + "!");   
                 } else {
                      System.out.println("Give me a  " + wordUpper.charAt(i) + ": " + wordUpper.charAt(i) + "!");
                 }
             }
-            System.out.println("What does that spell?");
+            System.out.println("What does that spell?"); // print the word, in uppercase, the amount of times specified 
             for (int j = 0; j < amount; j++){
                 System.out.println(wordUpper + "!!!");
             }

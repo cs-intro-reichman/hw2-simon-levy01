29c2f5ab96a954191f0b545aa4657bce1e734803
Simon Levy <simon.levy01@post.runi.ac.il>
2024-11-14 14:52:02 +0200

    finished assignments 2 and 3, uploaded now as no tests are done for 2

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,21 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+            int amount = Integer.parseInt(args[1]);
+            String wordUpper = args[0].toUpperCase();
+            int length = wordUpper.length();
+            String anLetters = "AEFHILMNORSX";
+            System.out.println(wordUpper + length );
+            for (int i = 0; i < length; i++){
+                if (anLetters.indexOf(wordUpper.charAt(i)) != -1){
+                     System.out.println("Give me an " + wordUpper.charAt(i) + ": " + wordUpper.charAt(i) + "!");   
+                } else {
+                     System.out.println("Give me a  " + wordUpper.charAt(i) + ": " + wordUpper.charAt(i) + "!");
+                }
+            }
+            System.out.println("What does that spell?");
+            for (int j = 0; j < amount; j++){
+                System.out.println(wordUpper + "!!!");
+            }
         }
 }
